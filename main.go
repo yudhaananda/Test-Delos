a77831c1dbb30bf8bb7827d3e15b1ffe14a5bc00
Yudha Ananda <yudha.ananda.kresna@bni.co.id>
2023-01-17 14:58:06 +0700

    remove private method

--- main.go
+++ main.go
@@ -8,8 +8,6 @@ import (
 	swaggerfiles "github.com/swaggo/files"
 	ginSwagger "github.com/swaggo/gin-swagger"
 
-	"time"
-
 	"github.com/gin-gonic/gin"
 )
 
@@ -31,51 +29,4 @@ func main() {
 	router.GET("swagger/*any", ginSwagger.WrapHandler(swaggerfiles.Handler))
 
 	router.Run()
-}
-
-func library(dueDate, submitDate time.Time) (result int) {
-	lateRange := submitDate.YearDay() - dueDate.YearDay()
-	if lateRange <= 0 || dueDate.Year() > submitDate.Year() {
-		result = 0
-		return
-	} else if submitDate.Year() != dueDate.Year() {
-		result = 12000
-		return
-	} else if submitDate.Month() != dueDate.Month() {
-		result = (int(submitDate.Month()) - int(dueDate.Month())) * 500
-		return
-	} else {
-		result = 15 * lateRange
-		return
-	}
-}
-
-func whoGetSourCandy(student, candies, firstStudent int) (result int) {
-	result = (candies + firstStudent - 1) % student
-	if result == 0 {
-		result = student
-	}
-	return
-}
-
-func sameSumElement(arrLen int, arr []int) (result string) {
-	for index := range arr {
-		left := 0
-		right := 0
-
-		for i := 0; i < arrLen; i++ {
-			if i > index {
-				right += arr[i]
-			}
-			if i < index {
-				left += arr[i]
-			}
-		}
-		if right == left {
-			result = "YES"
-			return
-		}
-	}
-	result = "NO"
-	return
 }
